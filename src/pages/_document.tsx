0ba7bd0c12eb61ed73117f11c6a3ce7eed44f907
Eduardo Mausa <mausaeduardo@gmail.com>
2022-09-05 19:37:02 -0300

    implement and style the Homepage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,23 @@
-import Document from 'next/document';
+import Document, {
+  Html,
+  Main,
+  Head,
+  NextScript,
+} from '../../node_modules/next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { GetStaticProps } from 'next';
-
+import next, { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,13 +24,93 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const blogTitle = '<MausaDev />';
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState<string | null>(
+    postsPagination.next_page
+  );
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  async function handleFetchMorePosts() {
+    if (nextPage) {
+      const response = await (await fetch(nextPage)).json();
 
-//   // TODO
-// };
+      const newPosts = response.results.map((post: Post) => ({
+        uid: post.uid,
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+
+        first_publication_date: post.first_publication_date,
+      }));
+
+      setNextPage(response.next_page);
+      setPosts(oldState => [...oldState, ...newPosts]);
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | Mausa Dev</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <h1>{blogTitle}</h1>
+        <div className={styles.content}>
+          {posts.map(post => (
+            <Link key={post.uid} href={`/posts/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p className={styles.subtitle}>{post.data.subtitle}</p>
+                <div className={styles.postInfo}>
+                  <time>{post.first_publication_date}</time>
+                  <p>{post.data.author}</p>
+                </div>
+              </a>
+            </Link>
+          ))}
+          <button type="button" onClick={handleFetchMorePosts}>
+            Carregar mais posts
+          </button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('post', { pageSize: 1 });
+
+  console.log(postsResponse)
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  const postsPagination = {
+    results: posts,
+    next_page: postsResponse.next_page,
+  };
+
+  return {
+    props: { postsPagination },
+    revalidate: 60 * 60 * 24, // 1 day
+  };
+};
